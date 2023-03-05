f91bca1e76b825d1148d539a3f0211e30919791f
Sumit Agrawal <sumitagrawal354@gmail.com>
2023-03-05 12:56:17 -0800

    Update settings.py

--- Project/Medical-Management-System/pharm/settings.py
+++ Project/Medical-Management-System/pharm/settings.py
@@ -29,8 +29,9 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['pharmacymanage.herokuapp.com']
 ALLOWED_HOSTS = [
-    'pharmacy-management-system2.herokuapp.com',
-    '127.0.0.1'
+    'localhost',
+    '127.0.0.1',
+    'Testingpharmacy-env.eba-bjf5t9in.us-west-2.elasticbeanstalk.com'
 ]
 
 # Application definition
@@ -88,23 +89,24 @@ WSGI_APPLICATION = 'pharm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'getmedsdb',
+    'USER': 'getmedsDB',
+    'PASSWORD': 'groupproject241',
+    'HOST': 'getmeds.cnbtewuf24zj.us-west-2.rds.amazonaws.com',
+    'PORT': '5432',
+  }
+}
+
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'pharmacyDb',
-#         'USER':'postgres',
-#         'PASSWORD':'1234',
-#         'HOST':'localhost'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
